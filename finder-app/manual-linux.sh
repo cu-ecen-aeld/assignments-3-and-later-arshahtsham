b77836c63d73575678d7d3e624e9d8777a5b5ad5
arsh <arsh@Ubuntu.myguest.virtualbox.org>
2024-07-22 18:18:53 -0400

    Updated finder.sh, manual-linux.sh, writer.sh, and added visudo

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -20,36 +20,36 @@ else
     echo "Using passed directory ${OUTDIR} for output"
 fi
 
-mkdir -p ${OUTDIR}
+sudo mkdir -p ${OUTDIR}  # Requires sudo if the directory is not writable by the user
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
-    # Clone only if the repository does not exist.
     echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-    git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+    sudo git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
-    git checkout ${KERNEL_VERSION}
+    sudo git checkout ${KERNEL_VERSION}
 
     # Apply manual fix for dtc issue
-    sed -i 's/YYLTYPE yylloc;/extern YYLTYPE yylloc;/' scripts/dtc/dtc-lexer.l
-    sed -i '/YYLTYPE yylloc;/a YYLTYPE yylloc;' scripts/dtc/dtc.c
+    # Ensure yylloc is declared extern in dtc-lexer.l if not already declared
+    sudo sed -i 's/YYLTYPE yylloc;/extern YYLTYPE yylloc;/' scripts/dtc/dtc-lexer.l
+    sudo sed -i '/YYLTYPE yylloc;/a YYLTYPE yylloc;' scripts/dtc/dtc.c
 
     # Kernel build steps
     echo "Configuring the kernel"
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    sudo make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
     echo "Building the kernel"
-    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    sudo make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
     echo "Building kernel modules"
-    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    sudo make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
     echo "Building device tree blobs"
-    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    sudo make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
-cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
+sudo cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -59,65 +59,50 @@ then
     sudo rm -rf ${OUTDIR}/rootfs
 fi
 
-mkdir -p ${OUTDIR}/rootfs
+sudo mkdir -p ${OUTDIR}/rootfs
 cd ${OUTDIR}/rootfs
-mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
-mkdir -p usr/bin usr/lib usr/sbin
-mkdir -p var/log
+sudo mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+sudo mkdir -p usr/bin usr/lib usr/sbin
+sudo mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
     echo "Cloning BusyBox"
-    git clone git://busybox.net/busybox.git
+    sudo git clone git://busybox.net/busybox.git
     cd busybox
-    git checkout ${BUSYBOX_VERSION}
+    sudo git checkout ${BUSYBOX_VERSION}
     # Configure BusyBox
     echo "Configuring BusyBox"
-    make distclean
-    make defconfig
-    if [ $? -ne 0 ]; then
-        echo "BusyBox defconfig failed"
-        exit 1
-    fi
+    sudo make distclean
+    sudo make defconfig
 else
     cd busybox
     # Ensure BusyBox is configured before building
     if [ ! -f .config ]; then
         echo "Configuring BusyBox"
-        make distclean
-        make defconfig
-        if [ $? -ne 0 ]; then
-            echo "BusyBox defconfig failed"
-            exit 1
-        fi
+        sudo make distclean
+        sudo make defconfig
     fi
 fi
 
 # Make and install BusyBox
-echo "Building BusyBox"
-make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-if [ $? -ne 0 ]; then
-    echo "BusyBox build failed"
-    exit 1
-fi
+echo "PATH before sudo make: $PATH"
+sudo env PATH=$PATH make -j$(nproc) ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE
+
 echo "Installing BusyBox"
-make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
-if [ $? -ne 0 ]; then
-    echo "BusyBox install failed"
-    exit 1
-fi
+sudo make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
+sudo ${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
+sudo ${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
 
 # Add library dependencies to rootfs
-SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
-cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
-cp ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
-cp ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
-cp ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
+SYSROOT=$(sudo ${CROSS_COMPILE}gcc -print-sysroot)
+sudo cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+sudo cp ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+sudo cp ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+sudo cp ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
 
 # Make device nodes
 echo "Creating device nodes"
@@ -127,27 +112,15 @@ sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
 # Clean and build the writer utility
 echo "Building writer utility"
 cd ${FINDER_APP_DIR}
-make clean
-make cross-compile CROSS_COMPILE=${CROSS_COMPILE}
+sudo make clean
+sudo make cross-compile CROSS_COMPILE=${CROSS_COMPILE}
 
-cp ${FINDER_APP_DIR}/writer.sh ${OUTDIR}/rootfs/home
-
-# Copy the finder related scripts and executables to the /home directory on the target rootfs
-echo "Copying files to rootfs"
-cp ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home
-cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home
-
-# Check if a file named 'conf' exists and remove it
-if [ -f "${OUTDIR}/rootfs/home/conf" ]; then
-    rm "${OUTDIR}/rootfs/home/conf"
-fi
-
-# Copy the 'conf' directory
-cp -r ${FINDER_APP_DIR}/conf ${OUTDIR}/rootfs/home
-
-cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home
-cp ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home
-
+sudo cp ${FINDER_APP_DIR}/writer.sh ${OUTDIR}/rootfs/home
+sudo cp ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home
+sudo cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home
+sudo cp -r ${FINDER_APP_DIR}/conf ${OUTDIR}/rootfs/home
+sudo cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home
+sudo cp ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home
 
 # Change ownership of the root directory to root
 echo "Changing ownership of rootfs"
@@ -156,8 +129,7 @@ sudo chown -R root:root ${OUTDIR}/rootfs
 # Create initramfs.cpio.gz
 echo "Creating initramfs"
 cd ${OUTDIR}/rootfs
-find . | cpio -H newc -o > ${OUTDIR}/initramfs.cpio
-gzip -f ${OUTDIR}/initramfs.cpio
+(cd ${OUTDIR}/rootfs && sudo sh -c 'find . | cpio -H newc -o | gzip -f > ../initramfs.cpio.gz')
 
 echo "Kernel and root filesystem build completed successfully."
 echo "Kernel image: ${OUTDIR}/Image"
